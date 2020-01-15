6799740a5d4cbe9048f52f2249a52862cebb4386
darosg1 <darosg1@yahoo.com>
2020-01-15 21:35:11 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,21 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
-    public class TestingMain {
-        public static void main(String[] args){
-            System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+public class TestingMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int resultA = calculator.addAToB(3, 2);
+        int resultS = calculator.subtractAFromB(5, 2);
+
+        if (resultA == (3 + 2)) {
+            System.out.println("Sum equals " + resultA + " - correct!");
+        } else {
+            System.out.println("Error");
+        }
+        if (resultS == (5 - 2)) {
+            System.out.println("Subtraction result equals " + resultS + " - correct!");
+        } else {
+            System.out.println("Error");
         }
     }
-
+}
